4633b41d563a9f6ac6b0f76e6dd602dc3d9a0cbe
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2018-04-11 19:56:11 -0400

    add blk api specific variables

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const port = process.env.port || 3002
 const clientId = process.env.CLIENT_ID
 const clientSecret = process.env.CLIENT_SECRET
 const subscriptionKey = process.env.EL_SUBSCRIPTION_KEY
+const deviceId = process.env.DEVICE_ID
+
+const redirectUri = 'http://localhost:3002/callback'
+const eventLogUri = 'https://api.buildinglink.com/EventLog/PropEmp/v1/Events'
+const tokenEndpoint = 'https://auth.buildinglink.com/connect/token'
+const grantType = 'authorization_code'
 
 const cors = require('cors')
 
